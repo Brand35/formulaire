d7c985067de8752217c710c86a5355aca83f8d77
Brand35 <brandou35000@gmail.com>
2024-07-30 09:13:05 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useForm } from "react-hook-form"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App =()=>{
+const {register, handleSubmit , formState: {errors}} = useForm();
+const dispatch= useDispatch();
+
+
+const onSubmit= (data)=>{
+ console.log(data);
+ dispatch({type: 'REGISTER_USER', payload: data});
+};
+return(
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <div>
+      <label>Nom :</label>
+      <input {...register('Nom', { required: true})}/>
+      {errors.Nom && <span>Ce champ est requis</span>}
     </div>
-  );
-}
+    <div>
+      <label>Prenom :</label>
+      <input {...register('Prenom', { required: true})}/>
+      {errors.Prenom && <span>Ce champ est requis</span>}
+    </div>
+    <div>
+      <label>Âge</label>
+      <input type='number' { ...register ('age', {required: true, validate: value  => value>18 || "Vous devez avoir plus de 18 ans"})}/>
+      {errors.age && <span>{errors.age.message}</span>}
+      </div>
+      <div>
+        <label>Email</label>
+        <input type='email' { ...register ('email', {required: true})}/>
+      {errors.email && <span>{errors.age.email}</span>}
+      </div>
+      <div>
+        <label>Mot de passe:</label>
+        <input type="password" {...register('password', { required: true })} />
+        {errors.password && <span>Ce champ est requis</span>}
+      </div>
+      <button type='submit'>"S'inscrire</button>
+  </form>
+);
+};
 
 export default App;
